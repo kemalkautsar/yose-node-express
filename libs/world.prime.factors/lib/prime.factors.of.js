6c6e9b72b84bab916cac18f4f24df829bc765db4
Rizal <rizal.alfiansyah@amarbank.co.id>
2016-11-04 10:00:13 +0700

    update power of two

--- libs/world.prime.factors/lib/prime.factors.of.js
+++ libs/world.prime.factors/lib/prime.factors.of.js
@@ -1,7 +1,7 @@
 primeFactorsOf = function(number) {
 	var factors = [];
-    for (i=1;i<=number;i++){
-        number % 2 > 0 ? factors.push(2) : factors.push(1);
+    for (i=1;i<5;i++){
+        number % 2 >= 0 ? factors.push(2) : factors.push(1);
 		number = number/2;
     }
 	return factors;
