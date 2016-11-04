623d4e20ad7a87f5884cbafd3cd60d5def0199bb
Rizal <rizal.alfiansyah@amarbank.co.id>
2016-11-04 14:16:21 +0700

    fix null string guard

--- libs/world.prime.factors/prime.factors.js
+++ libs/world.prime.factors/prime.factors.js
@@ -3,7 +3,7 @@ var primeFactorsOf = require('./lib/prime.factors.of');
 var primeFactors = function(request, response) {
 	var number = parseInt(request.query['number']);
     var decomposition = primeFactorsOf(number);
-	var result = isNaN(number) ? {number: request.query['number'], error: "not a number"} : {number: number, decomposition: decomposition};
+	var result = isNaN(number) ? {number: request.query['number'] == "" ? null : request.query['number'], error: "not a number"} : {number: number, decomposition: decomposition};
 	response.setHeader('Content-Type', 'application/json');
 
     response.send(JSON.stringify(result));
